25f8d5b52eb4ab6e47bf240c50c0f0d196acae70
stephen nene <stevekid705@gmail.com>
2023-07-09 14:21:03 +0300

    new login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import App from './app/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
