9c2a09d8b9f5b3ead905f946cdd08548c4865df4
stephen nene <stevekid705@gmail.com>
2023-07-10 09:54:07 +0300

    create users works well

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       render json: @user, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -46,6 +47,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :email, :password_digest)
+      params.permit(:username, :email, :password)
     end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
 
-  get    '/me',   to: 'sessions#new'
+  get    '/me',   to: 'sessions#find'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
