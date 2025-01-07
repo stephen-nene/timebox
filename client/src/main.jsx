2b987959d848d3fe67e89a837a9d0e680c6db925
Stephen Nene <stephen.nene@student.moringaschool.com>
2025-01-07 15:10:29 +0300

    Enable Analytics component and update package dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <React.StrictMode>
         <App />
-        {/* <Analytics /> */}
+        <Analytics />
       </React.StrictMode>
     </Provider>
   </BrowserRouter>
